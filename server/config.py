c7ba8077b31cf90e7270c59ea27d23318e0ad2ce
Ternario <andrii.shevchenko.6.9.6@gmail.com>
2026-08-01 15:22:43 +0200

    set base config data, test cd

---
+++ server/config.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    MYSQL_USER: str
+    MYSQL_PASSWORD: str
+    MYSQL_DATABASE: str
+    DB_HOST: str
+    DB_PORT: int
+
+    @property
+    def DATABASE_URL(self) -> str:
+        return (
+            f'mysql+asyncmy://'
+            f'{self.MYSQL_USER}:'
+            f'{self.MYSQL_PASSWORD}@'
+            f'{self.DB_HOST}:'
+            f'{self.DB_PORT}'
+            f'{self.MYSQL_DATABASE}'
+        )
+
+    BASE_URL: str
+    UPLOAD_DIR: Path = Path('static/app')
+
+    model_config = SettingsConfigDict(
+        env_file=('.env.database', '.env.server')
+    )
+
+
+settings = Settings()
---
+++ server/db/engine.py
@@ -0,0 +1,7 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
+
+from config import settings
+
+engine = create_async_engine(settings.DATABASE_URL, echo=True, pool_pre_ping=True)
+
+async_session = async_sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
